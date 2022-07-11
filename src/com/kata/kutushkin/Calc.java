58bc87dbabd58a1d22f15906d651573d3487e63d
AlexandrKutushkin <alexandr.kutushkin@yandex.ru>
2022-07-12 00:09:26 +0300

    - Добавлена проверка на ввод чисел меньше 1 или больше 10; - Добавлена функция  public static String calc(String input)  в соответствии с ТЗ.

--- src/com/kata/kutushkin/Calc.java
+++ src/com/kata/kutushkin/Calc.java
@@ -9,10 +9,8 @@ public class Calc {
     static char operation;
     static boolean arabian;
     static int answer;
-
-    public static void main(String[] args) throws userInputException, notATaskException, digitCombinationException, romeBelowZeroException {
-        ArrayList<String> rome = new ArrayList<>();
-
+    static  ArrayList<String> rome = new ArrayList<>();
+    static {
         rome.add("I");
         rome.add("II");
         rome.add("III");
@@ -23,11 +21,9 @@ public class Calc {
         rome.add("VIII");
         rome.add("IX");
         rome.add("X");
-
-
-        System.out.println("Введите пример(разделяя пробелами числа и оператор)");
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+    }
+    public static String calc(String input) throws romeBelowZeroException, digitCombinationException, userInputException, notATaskException, wrongInputDigitValueException {
+        String toOut = null;
         String[] array = input.split(" ");
 
         for (String strForEach : array) {
@@ -35,7 +31,7 @@ public class Calc {
                 throw new userInputException("throws Exception // недопустимые символы");
         }                                           //проверка на введеные символы
 
-                                                                                        //проверка на длину и запись
+        //проверка на длину и запись
         if ((array.length != 3) || (!array[1].matches("[+*/-]")) || (!(array[0].matches("[IXV0-9]+") & (array[2].matches("[IXV0-9]+"))))) {
             throw new notATaskException("throws Exception // строка не является математической операцией");
         }
@@ -46,17 +42,18 @@ public class Calc {
             throw new digitCombinationException("Вы пытаетесь выполнить действие с числами разных систем счисления");
         }  //проверка комбинации разных СС
 
+
         //решаем
 
         if (array[0].matches("[0-9]+"))
             arabian = true;
-            operation = array[1].charAt(0);
+        operation = array[1].charAt(0);
 
         if (arabian) {
             a = Integer.parseInt(array[0]);
             b = Integer.parseInt(array[2]);
             answer = getAnswer(a, b);
-            System.out.println(answer);
+            toOut = answer+"";
         }
 
         else {
@@ -69,7 +66,7 @@ public class Calc {
 //            System.out.println(answer);               //вывод арабскими цифрами
 
             if (answer == 100)
-                System.out.println("C");
+               toOut = "C";
             else {
 
                 switch (answer / 10) {
@@ -86,13 +83,26 @@ public class Calc {
 
 
                 }
-                System.out.println(romeAnswer);
+                toOut = romeAnswer;
             }
         }
 
+
+        return toOut;
     }
 
-    static int getAnswer(int first, int second) {
+    public static void main(String[] args) throws userInputException, notATaskException, digitCombinationException, romeBelowZeroException, wrongInputDigitValueException {
+        System.out.println("Введите пример(разделяя пробелами числа и оператор)");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(calc(input));
+
+    }
+
+    static int getAnswer(int first, int second) throws wrongInputDigitValueException {
+        if((first<1|first>10)|(second<1|second>10)){
+            throw new wrongInputDigitValueException("Вы ввели число меньше 1 или больше 10");
+        }
         switch (operation) {
             case '+' -> answer = a + b;
             case '-' -> answer = a - b;
@@ -120,6 +130,11 @@ class romeBelowZeroException extends Exception{
 }
 class digitCombinationException extends Exception{
     public digitCombinationException (String info){
+        super(info);
+    }
+}
+class wrongInputDigitValueException extends Exception{
+    public wrongInputDigitValueException (String info){
         super(info);
     }
 }
